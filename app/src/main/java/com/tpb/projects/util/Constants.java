e9d0d107cdeaa3eabdd277a20bf740646be3db81
tpb1908 <theo1908phone@gmail.com>
2016-12-14 22:03:46 +0000

    Added client id and secret to BuildConfig. Placed login fields in CardView. Updated LoginActivity (Removing unneeded stuff and replacing string resources. Updated dependencies.

---
+++ app/src/main/java/com/tpb/projects/util/Constants.java
@@ -0,0 +1,13 @@
+package com.tpb.projects.util;
+
+import com.tpb.projects.BuildConfig;
+
+/**
+ * Created by theo on 14/12/16.
+ */
+
+public class Constants {
+    public static final String CLIENT_ID = BuildConfig.GITHUB_CLIENT_ID;
+    public static final String SECRET = BuildConfig.GITHUB_CLIENT_SECRET;
+
+}
