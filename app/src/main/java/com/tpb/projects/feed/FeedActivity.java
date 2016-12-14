1e84cbc371ba364565155a02b1681ffa78bd8b55
tpb1908 <theo1908phone@gmail.com>
2016-12-14 16:59:32 +0000

    Added dependencies and two modules.

---
+++ app/src/main/java/com/tpb/projects/feed/FeedActivity.java
@@ -0,0 +1,10 @@
+package com.tpb.projects.feed;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by theo on 14/12/16.
+ */
+
+public class FeedActivity extends AppCompatActivity {
+}
