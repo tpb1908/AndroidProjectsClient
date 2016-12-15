988a74aecf34fd6491e8c2eaf168bc16f789dbb2
tpb1908 <theo1908phone@gmail.com>
2016-12-15 18:23:04 +0000

    Set up github authorization.

--- app/src/main/java/com/tpb/projects/util/Constants.java
+++ app/src/main/java/com/tpb/projects/util/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String GIT_API_ROOT_URL = "https://api.github.com/";
     public static final String AUTH_SERVER_URL = "https://github.com/login/oauth/authorize";
     public static final String TOKEN_SERVER_URL = "https://github.com/login/oauth/access_token";
-    public static final String REDIRECT_URL = "http://localhost/Callback";
+    public static final String REDIRECT_URL = "https://github.com/tpb1908/AndroidProjectsClient";
     public static final  String CREDENTIALS_STORE_PREF_FILE = "oauth";
 
 
