f902e93ebe5cc27a77985cdd8ebec0e10766e9ac
tpb1908 <theo1908phone@gmail.com>
2016-12-15 22:30:15 +0000

    Added repo model.

---
+++ app/src/main/java/com/tpb/projects/data/auth/models/Repository.java
@@ -0,0 +1,106 @@
+package com.tpb.projects.data.auth.models;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by theo on 15/12/16.
+ */
+
+public class Repository {
+    private static final String TAG = Repository.class.getSimpleName();
+
+    private Repository() {}
+
+    private static final String ID = "id";
+    private int id;
+
+    private static final String NAME = "name";
+    private String name;
+
+    private static final String FULL_NAME = "full_name";
+    private String fullName;
+
+    private static final String DESCRIPTION = "description";
+    private String description;
+
+    private static final String PRIVATE = "private";
+    private boolean isPrivate;
+
+    private static final String FORK = "fork";
+    private boolean isFork;
+
+    private static final String URL = "url";
+    private String url;
+
+    private static final String HTML_URL = "html_url";
+    private String htmlUrl;
+
+    private static final String LANGUAGE = "language";
+    private String language;
+
+    private static final String ISSUES = "has_issues";
+    private boolean hasIssues;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public boolean isFork() {
+        return isFork;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public boolean isHasIssues() {
+        return hasIssues;
+    }
+
+    public static Repository parse(JSONObject object) {
+        final Repository r = new Repository();
+        try {
+            r.id = object.getInt(ID);
+            r.name = object.getString(NAME);
+            r.fullName = object.getString(FULL_NAME);
+            r.description = object.getString(DESCRIPTION);
+            r.isPrivate = object.getBoolean(PRIVATE);
+            r.isFork = object.getBoolean(FORK);
+            r.url = object.getString(URL);
+            r.htmlUrl = object.getString(HTML_URL);
+            r.language = object.getString(LANGUAGE);
+            r.hasIssues = object.getBoolean(ISSUES);
+        } catch(JSONException jse) {
+            Log.e(TAG, "parse: ", jse);
+        }
+
+        return r;
+    }
+}
