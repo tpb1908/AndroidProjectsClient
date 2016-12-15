537d6136da575950a79d21e9a58cb091dad0dd28
tpb1908 <theo1908phone@gmail.com>
2016-12-15 22:39:47 +0000

    Added models for Project, Column, and Card. Parsing not yet implemented.

---
+++ app/src/main/java/com/tpb/projects/data/auth/models/Project.java
@@ -0,0 +1,38 @@
+package com.tpb.projects.data.auth.models;
+
+/**
+ * Created by theo on 15/12/16.
+ */
+
+public class Project {
+
+    private Project() {}
+
+    private static final String OWNER_URL = "owner_url";
+    private String ownerUrl;
+
+    private static final String URL = "url";
+    private String url;
+
+    private static final String NAME = "name";
+    private String name;
+
+    private static final String BODY = "body";
+    private String body;
+
+    private String NUMBER = "number";
+    private int number;
+
+    private static final String CREATOR = "creator";
+    private int owner;
+
+    private static final String CREATED_AT = "created_at";
+    private long createdAt;
+
+    private static final String UPDATED_AT = "updated_at";
+    private long updatedAt;
+
+
+
+
+}
