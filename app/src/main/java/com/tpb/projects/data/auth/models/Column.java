8c030baa1995680c48c65db4cf3ba604a7113534
tpb1908 <theo1908phone@gmail.com>
2016-12-16 13:46:11 +0000

    Added abstract DataModel and parse methods to other models.

--- app/src/main/java/com/tpb/projects/data/auth/models/Column.java
+++ app/src/main/java/com/tpb/projects/data/auth/models/Column.java
@@ -1,17 +1,21 @@
 package com.tpb.projects.data.auth.models;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by theo on 15/12/16.
  */
 
-public class Column {
+public class Column extends DataModel {
+    private static final String TAG = Column.class.getSimpleName();
 
     private Column() {}
 
-    private static final String ID = "id";
     private int id;
 
-    private static final String NAME = "name";
     private String name;
 
     private static final String PROJECT_URL = "project_url";
@@ -23,6 +27,36 @@ public class Column {
     private static final String UPDATED_AT = "updated_at";
     private long updatedAt;
 
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getProjectUrl() {
+        return projectUrl;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public static Column parse(JSONObject object) {
+        final Column c = new Column();
+        try {
+            c.id = object.getInt(ID);
+            c.name = object.getString(NAME);
+            c.projectUrl = object.getString(PROJECT_URL);
+        } catch(JSONException jse) {
+            Log.e(TAG, "parse: ", jse);
+        }
+        return c;
+    }
 
 }
---
+++ app/src/main/java/com/tpb/projects/data/auth/models/DataModel.java
@@ -0,0 +1,16 @@
+package com.tpb.projects.data.auth.models;
+
+/**
+ * Created by theo on 15/12/16.
+ */
+
+public abstract class DataModel {
+
+    static final String ID = "id";
+    static final String NAME = "name";
+    static final String CREATED_AT = "created_at";
+    static final String UPDATED_AT = "updated_at";
+    static final String URL = "url";
+
+
+}
