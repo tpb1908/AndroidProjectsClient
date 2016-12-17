a88f47ab3ea722664cd2569e61888f540e4848a3
tpb1908 <theo1908phone@gmail.com>
2016-12-17 18:55:11 +0000

    Implemented loading of Projects in RepoActivity.

---
+++ app/src/main/java/com/tpb/projects/views/AnimatingRecycler.java
@@ -0,0 +1,10 @@
+package com.tpb.projects.views;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by theo on 17/12/16.
+ */
+
+public class AnimatingRecycler extends RecyclerView {
+}
