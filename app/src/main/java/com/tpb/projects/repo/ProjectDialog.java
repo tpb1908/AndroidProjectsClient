b3c7d1fe9074d2e8ee07200efc4e58c555ac8985
tpb1908 <theo1908phone@gmail.com>
2016-12-18 15:04:43 +0000

    Added animations to dialog enter and exit.

--- app/src/main/java/com/tpb/projects/repo/ProjectDialog.java
+++ app/src/main/java/com/tpb/projects/repo/ProjectDialog.java
@@ -79,6 +79,9 @@ public class ProjectDialog extends DialogFragment {
         return builder.create();
     }
 
-
-
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getDialog().getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+    }
 }
