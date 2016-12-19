9df9de4dace0abf29752c5c828fb8a440d8b7e7b
tpb1908 <theo1908phone@gmail.com>
2016-12-19 21:23:33 +0000

    Added double fab for new cards or columns.

--- app/src/main/java/com/tpb/projects/project/ProjectActivity.java
+++ app/src/main/java/com/tpb/projects/project/ProjectActivity.java
@@ -2,9 +2,7 @@ package com.tpb.projects.project;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -32,7 +30,7 @@ import butterknife.ButterKnife;
 public class ProjectActivity extends AppCompatActivity implements Loader.ProjectLoader {
 
     @BindView(R.id.project_name) TextView mName;
-    @BindView(R.id.project_new_card_fab) FloatingActionButton mNewCardFab;
+    //@BindView(R.id.project_new_card_fab) FloatingActionButton mNewCardFab;
     @BindView(R.id.project_column_pager) ViewPager mColumnPager;
 
     private ColumnPager mAdapter;
@@ -54,7 +52,7 @@ public class ProjectActivity extends AppCompatActivity implements Loader.Project
         mColumnPager.setAdapter(mAdapter);
         mColumnPager.setOffscreenPageLimit(mAdapter.getCount());
 
-        new Handler().postDelayed(() -> mNewCardFab.show(), 400);
+       // new Handler().postDelayed(() -> mNewCardFab.show(), 400);
     }
 
     @Override
@@ -81,7 +79,7 @@ public class ProjectActivity extends AppCompatActivity implements Loader.Project
 
     @Override
     public void onBackPressed() {
-        mNewCardFab.hide();
+     //   mNewCardFab.hide();
         super.onBackPressed();
     }
 
