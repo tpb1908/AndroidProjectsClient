f63e8e52a60ea0c99e9b7d869409e66cfbab9f45
tpb1908 <theo1908phone@gmail.com>
2016-12-31 19:42:31 +0000

    Added loading dialog for issue dialog. Resolves #30 .

--- app/src/main/java/com/tpb/projects/project/dialogs/CardDialog.java
+++ app/src/main/java/com/tpb/projects/project/dialogs/CardDialog.java
@@ -18,6 +18,7 @@
 package com.tpb.projects.project.dialogs;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -73,43 +74,50 @@ public class CardDialog extends DialogFragment {
 
             final View issueButton = view.findViewById(R.id.card_from_issue_button);
             issueButton.setVisibility(View.VISIBLE);
-            issueButton.setOnClickListener(view1 -> new Loader(getContext()).loadOpenIssues(new Loader.IssuesLoader() {
-                private int selectedIssuePosition = 0;
-                @Override
-                public void issuesLoaded(Issue[] loadedIssues) {
-                    if(loadedIssues.length == 0) {
-                        Toast.makeText(getContext(), R.string.error_no_issues, Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    final ArrayList<Issue> validIssues = new ArrayList<>();
-                    for(Issue i : loadedIssues) {
-                        if(invalidIds.indexOf(i.getId()) == -1) validIssues.add(i);
-                    }
-                    final String[] texts = new String[validIssues.size()];
-                    for(int i = 0; i < validIssues.size(); i++) {
-                        texts[i] = String.format(getContext().getString(R.string.text_issue_single_line),
-                                validIssues.get(i).getNumber(), validIssues.get(i).getTitle());
+            issueButton.setOnClickListener(view1 -> {
+                final ProgressDialog pd = new ProgressDialog(getContext());
+                pd.setTitle(R.string.text_loading_issues);
+                pd.setCancelable(false);
+                pd.show();
+                new Loader(getContext()).loadOpenIssues(new Loader.IssuesLoader() {
+                    private int selectedIssuePosition = 0;
+                    @Override
+                    public void issuesLoaded(Issue[] loadedIssues) {
+                        pd.dismiss();
+                        if(loadedIssues.length == 0) {
+                            Toast.makeText(getContext(), R.string.error_no_issues, Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        final ArrayList<Issue> validIssues = new ArrayList<>();
+                        for(Issue i : loadedIssues) {
+                            if(invalidIds.indexOf(i.getId()) == -1) validIssues.add(i);
+                        }
+                        final String[] texts = new String[validIssues.size()];
+                        for(int i = 0; i < validIssues.size(); i++) {
+                            texts[i] = String.format(getContext().getString(R.string.text_issue_single_line),
+                                    validIssues.get(i).getNumber(), validIssues.get(i).getTitle());
+                        }
+
+                        final AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
+                        builder1.setTitle(R.string.title_choose_issue);
+                        builder1.setSingleChoiceItems(texts, 0, (dialogInterface, i) -> {
+                            selectedIssuePosition = i;
+                        });
+                        builder1.setPositiveButton(R.string.action_ok, ((dialogInterface, i) -> {
+                            if(mListener != null) mListener.issueCardCreated(validIssues.get(selectedIssuePosition));
+                            dialogInterface.dismiss();
+                            CardDialog.this.dismiss();
+                        }));
+                        builder1.setNegativeButton(R.string.action_cancel, (dialogInterface, i) -> dialogInterface.dismiss());
+                        builder1.create().show();
                     }
 
-                    final AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
-                    builder1.setTitle(R.string.title_choose_issue);
-                    builder1.setSingleChoiceItems(texts, 0, (dialogInterface, i) -> {
-                        selectedIssuePosition = i;
-                    });
-                    builder1.setPositiveButton(R.string.action_ok, ((dialogInterface, i) -> {
-                       if(mListener != null) mListener.issueCardCreated(validIssues.get(selectedIssuePosition));
-                        dialogInterface.dismiss();
-                        CardDialog.this.dismiss();
-                    }));
-                    builder1.setNegativeButton(R.string.action_cancel, (dialogInterface, i) -> dialogInterface.dismiss());
-                    builder1.create().show();
-                }
-
-                @Override
-                public void issuesLoadError() {
-
-                }
-            }, fullRepoName));
+                    @Override
+                    public void issuesLoadError() {
+                        pd.dismiss();
+                    }
+                }, fullRepoName);
+            });
             //TODO Allow choosing an open issue
         }
 
