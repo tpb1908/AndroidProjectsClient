4e784222fc78ba7c12b84c9b026e25ddbb45cd03
tpb1908 <theo1908phone@gmail.com>
2017-02-16 11:06:58 +0000

    Moved image loading to abstract ImageLoadingActivity.

--- app/src/main/java/com/tpb/projects/editors/CommentEditor.java
+++ app/src/main/java/com/tpb/projects/editors/CommentEditor.java
@@ -1,40 +1,26 @@
 package com.tpb.projects.editors;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Base64;
-import android.util.Log;
 import android.view.ViewStub;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.tpb.projects.R;
 import com.tpb.projects.data.SettingsActivity;
 import com.tpb.projects.data.models.Comment;
 import com.tpb.projects.data.models.Issue;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -44,7 +30,7 @@ import butterknife.OnClick;
  * Created by theo on 14/02/17.
  */
 
-public class CommentEditor extends AppCompatActivity {
+public class CommentEditor extends ImageLoadingActivity {
     private static final String TAG = CommentEditor.class.getSimpleName();
 
     public static final int REQUEST_CODE_NEW_COMMENT = 1799;
@@ -121,6 +107,7 @@ public class CommentEditor extends AppCompatActivity {
                 return mEditor.getText().toString();
             }
         });
+
     }
 
     @OnClick(R.id.markdown_editor_done)
@@ -141,97 +128,14 @@ public class CommentEditor extends AppCompatActivity {
         //onBackPressed();
     }
 
-    private void showDialog() {
-        final CharSequence[] items = {"Take a picture", "Choose from gallery", "Cancel"};
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Upload an image");
-        builder.setItems(items, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if (items[which].equals(items[0])) {
-                    attemptTakePicture();
-                } else if (items[which].equals(items[1])) {
-                    final Intent intent = new Intent(
-                            Intent.ACTION_GET_CONTENT,
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(intent, SELECT_FILE);
+    @Override
+    void imageLoadComplete(String image64, ProgressDialog dialog) {
 
-                } else if (items[which].equals(items[2])) {
-                    dialog.dismiss();
-                }
-            }
-        });
-        builder.show();
-    }
-
-    private void attemptTakePicture() {
-        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch(IOException ioe) {
-                Log.e(TAG, "attemptTakePicture: ", ioe);
-            }
-
-            if(photoFile != null) {
-                final Uri photoURI = FileProvider.getUriForFile(this, "com.tpb.projects.provider", photoFile);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                Log.i(TAG, "attemptTakePicture: File created");
-                startActivityForResult(intent, REQUEST_CAMERA);
-            } else {
-                Log.i(TAG, "attemptTakePicture: File is null");
-            }
-        } else {
-            Log.i(TAG, "onClick: No camera to use");
-        }
-    }
-
-    private File createImageFile() throws IOException {
-        final String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        final String imageFileName = "JPEG_" + timeStamp + "_";
-        final File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        final File image =  File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        mCurrentFilePath = image.getAbsolutePath();
-        return image;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == AppCompatActivity.RESULT_OK) {
-            if(requestCode == REQUEST_CAMERA) {
-                Log.i(TAG, "onActivityResult: Camera request returned");
-                Toast.makeText(this, mCurrentFilePath, Toast.LENGTH_LONG).show();
+    void imageLoadException(IOException ioe) {
 
-                final Bitmap image = BitmapFactory.decodeFile(mCurrentFilePath);
-                Log.i(TAG, "onActivityResult: Image is: " + image.toString());
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                image.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                byte[] array = stream.toByteArray();
-                Log.i(TAG, "onActivityResult: Array length " + array.length);
-                String base64 = Base64.encodeToString(array, Base64.DEFAULT);
-                if(base64.length() > 4000) {
-                    Log.v(TAG, "base64.length = " + base64.length());
-                    int chunkCount = base64.length() / 4000;     // integer division
-                    for(int i = 0; i <= chunkCount; i++) {
-                        int max = 4000 * (i + 1);
-                        if(max >= base64.length()) {
-                            Log.v(TAG, "chunk " + i + " of " + chunkCount + ":" + base64.substring(4000 * i));
-                        } else {
-                            Log.v(TAG, "chunk " + i + " of " + chunkCount + ":" + base64.substring(4000 * i, max));
-                        }
-                    }
-                }
-            } else if(requestCode == SELECT_FILE) {
-                Log.i(TAG, "onActivityResult: File request returned");
-            }
-        }
     }
 
     @Override
