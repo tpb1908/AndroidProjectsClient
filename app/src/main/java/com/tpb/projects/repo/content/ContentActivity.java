de5407e70ad9e89b2e42aa0ea3108678082fa832
tpb1908 <theo1908phone@gmail.com>
2017-02-17 21:56:28 +0000

    Added layouts for ContentAdapter and content ribbon. Added further skeleton to ContentAdapter.

--- app/src/main/java/com/tpb/projects/repo/content/ContentActivity.java
+++ app/src/main/java/com/tpb/projects/repo/content/ContentActivity.java
@@ -3,8 +3,14 @@ package com.tpb.projects.repo.content;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.tpb.projects.R;
 import com.tpb.projects.data.APIHandler;
@@ -14,6 +20,7 @@ import com.tpb.projects.data.models.files.Node;
 
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -23,6 +30,11 @@ import butterknife.ButterKnife;
 public class ContentActivity extends AppCompatActivity {
     private static final String TAG = ContentActivity.class.getSimpleName();
 
+    @BindView(R.id.content_file_ribbon) LinearLayout mRibbon;
+    @BindView(R.id.content_recycler) RecyclerView mRecycler;
+    @BindView(R.id.content_refresher) SwipeRefreshLayout mRefresher;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +43,13 @@ public class ContentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_content);
         ButterKnife.bind(this);
 
+        initRibbon();
+
         final Intent launchIntent = getIntent();
         final String repo = launchIntent.getStringExtra(getString(R.string.intent_repo));
+
+        mRecycler.setLayoutManager(new LinearLayoutManager(this));
+
         new FileLoader(this).loadDirectory(new FileLoader.DirectoryLoader() {
             @Override
             public void directoryLoaded(List<Node> directory) {
@@ -44,5 +61,23 @@ public class ContentActivity extends AppCompatActivity {
                 Log.i(TAG, "directoryLoadError: " + error);
             }
         }, repo, null);
+    }
+
+    private void initRibbon() {
+        final TextView view = (TextView) getLayoutInflater().inflate(R.layout.shard_ribbon_item, mRibbon, false);
+        view.setText("Root");
+        mRibbon.addView(view);
+    }
+
+    private void addRibbonItem(Node node) {
+
+    }
+
+    private void removeRibbonItems(Node node) {
+
+    }
+
+    public void onToolbarBackPressed(View view) {
+        onBackPressed();
     }
 }
--- app/src/main/java/com/tpb/projects/repo/content/ContentAdapter.java
+++ app/src/main/java/com/tpb/projects/repo/content/ContentAdapter.java
@@ -1,32 +1,57 @@
 package com.tpb.projects.repo.content;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.tpb.projects.R;
 import com.tpb.projects.data.models.files.Node;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by theo on 17/02/17.
  */
 
-public class ContentAdapter extends RecyclerView.Adapter {
+public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.NodeViewHolder> {
 
-    private ArrayList<Node> root = new ArrayList<>();
+    private List<Node> root = new ArrayList<>();
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public NodeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new NodeViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_node, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(NodeViewHolder holder, int position) {
 
+    }
+
+    void setNodes(List<Node> nodes) {
+        root = nodes;
+        notifyDataSetChanged();
+    }
+
+    void appendNode(Node parent, List<Node> node) {
+        //TODO Traverse and insert
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return root.size();
     }
+
+    class NodeViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mText;
+
+        public NodeViewHolder(View itemView) {
+            super(itemView);
+            mText = (TextView) itemView.findViewById(R.id.node_text);
+        }
+    }
+
 }
