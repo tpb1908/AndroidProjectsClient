122f916b45294208dc71af56d9ed15509a281843
tpb1908 <theo1908phone@gmail.com>
2017-02-25 21:01:31 +0000

    Added layout stub for milestone_editor.

---
+++ app/src/main/java/com/tpb/projects/editors/MilestoneEditor.java
@@ -0,0 +1,17 @@
+package com.tpb.projects.editors;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by theo on 25/02/17.
+ */
+
+public class MilestoneEditor extends CircularRevealActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
