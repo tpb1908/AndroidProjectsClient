3964885c38f006852cd9996ec6776bd84c7913dd
tpb1908 <theo1908phone@gmail.com>
2017-03-14 11:20:23 +0000

    Added IssueCommentsAdapter and IssueCommentsFragment.

---
+++ app/src/main/java/com/tpb/projects/issues/content/IssueFragment.java
@@ -0,0 +1,10 @@
+package com.tpb.projects.issues.content;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by theo on 14/03/17.
+ */
+
+public abstract class IssueFragment extends Fragment {
+}
