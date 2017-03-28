132d2f4fb8a018f2deb1f45876114cf04b68ffc0
tpb1908 <theo1908phone@gmail.com>
2017-03-28 21:37:34 +0100

    Fixed CardEditor formatting of Issues without a body.

--- app/src/main/java/com/tpb/projects/markdown/Spanner.java
+++ app/src/main/java/com/tpb/projects/markdown/Spanner.java
@@ -32,8 +32,8 @@ public class Spanner {
 
         if(issue.getBody() != null && issue.getBody().trim().length() > 0) {
             builder.append(Markdown.formatMD(issue.getBody().replaceFirst("\\s++$", ""), issue.getRepoPath()));
-            builder.append("\n\n");
         }
+        builder.append("\n\n");
         if(showNumberedLink) {
             builder.append(String.format(context.getString(R.string.text_issue_opened_by),
                     String.format(context.getString(R.string.text_md_link),
